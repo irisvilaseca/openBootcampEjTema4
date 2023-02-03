539759373ef6d594fd515da76278b6cdcc866fd9
Iris <iris.vilaseca1998@gmail.com>
2023-02-03 19:27:26 +0100

    conditional and while activities done

--- src/org/openbootcamp/Main.java
+++ src/org/openbootcamp/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int numeroIf;
+        if(numeroIf>0){
+            System.out.println("La variable es un número positivo");
+        }else if(numeroIf<0){
+            System.out.println("La variable es un número negativo");
+        }else{
+            System.out.println("La variable es cero");
+        }
     }
 }
