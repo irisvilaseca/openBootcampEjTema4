03e4ac78323f1cd8a06bd0cfb040c15556401162
Iris <iris.vilaseca1998@gmail.com>
2023-02-03 19:34:14 +0100

    doWhile and for loop activities done

--- src/org/openbootcamp/Main.java
+++ src/org/openbootcamp/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int numeroIf;
+        int numeroIf=0;
         if(numeroIf>0){
             System.out.println("La variable es un número positivo");
         }else if(numeroIf<0){
@@ -12,5 +12,21 @@ public class Main {
         }else{
             System.out.println("La variable es cero");
         }
+        int numeroWhile=0;
+        while(numeroWhile<3){
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+
+        int numeroDoWhile=3;
+        do{
+            numeroDoWhile++;
+            System.out.println(numeroDoWhile);
+        }while(numeroDoWhile<3);
+
+        for(int numeroFor=0;numeroFor<=3;numeroFor++){
+            System.out.println(numeroFor);
+        }
+
     }
 }
